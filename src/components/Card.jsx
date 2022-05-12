f8faddf17fc82c4a48e39c9f6c2b137de9cba970
ParthaChangmai <partha.partha.changmai@gmail.com>
2022-05-12 05:58:17 +0530

    css changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,30 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({ animeData }) => {
+	console.log(animeData);
 	return (
 		<>
-			<div className="flex flex-col mb-6 h-72 w-60 sm:mt-11 rounded-lg shadow-md shadow-white text-white bg-gray-900">
+			<div className="flex flex-col mb-6 h-[320px] w-60 sm:mt-5 rounded-lg shadow-md shadow-white text-white bg-gray-900">
 				<a href="#!">
 					<img
-						className="rounded-t-lg "
-						src="https://mdbootstrap.com/img/new/standard/nature/184.jpg"
+						className="rounded-t-lg h-[200px] w-60 outline-none"
+						src={animeData.images.webp.image_url}
 						alt=""
 					/>
 				</a>
 				<div className="p-2">
-					<h5 className=" text-xl font-bold mb-2">Card title</h5>
+					<h5 className="font-bold pb-2">
+						{animeData?.title?.length > 22
+							? `${animeData.title.substring(0, 22)}...`
+							: animeData.title}
+					</h5>
 					<p className=" text-base mb-4">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						{animeData?.synopsis.substring(0, 48)}...
 					</p>
+
 					<button
 						type="button"
-						className=" inline-block px-4 py-2.5 bg-gray-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-gray-700 hover:shadow-lg focus:bg-gray-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blugray-800 active:shadow-lg transition duration-150 ease-in-out"
+						className=" items-center inline-block px-4 py-2.5 bg-gray-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-gray-700 hover:shadow-lg focus:bg-gray-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blugray-800 active:shadow-lg transition duration-150 ease-in-out"
 					>
 						More Details
 					</button>
