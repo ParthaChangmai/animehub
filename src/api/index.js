f305b1466a3503fef1cca58fcb5c517e33c6d204
ParthaChangmai <partha.partha.changmai@gmail.com>
2022-05-12 05:58:09 +0530

    fixed the api call

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,20 @@
 import axios from 'axios';
 
-export const getAnime = async (q) => {
+export const getAnime = async (q = 1) => {
 	try {
-		const res = await axios(`https://api.jikan.moe/v4/top/anime `, {
-			params: {
-				q: q,
-			},
-		});
+		const res = await axios(
+			`https://api.jikan.moe/v4/top/anime?page=${q} `,
+			{}
+		);
+
+		return res.data.data;
+	} catch (error) {
+		console.log(error);
+	}
+};
+export const getUpcomingAnime = async () => {
+	try {
+		const res = await axios(`https://api.jikan.moe/v4/seasons/now`);
 
 		return res.data.data;
 	} catch (error) {
