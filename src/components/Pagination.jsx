d51f6d0f970cca96a48735b36b3e2257571b822e
ParthaChangmai <partha.partha.changmai@gmail.com>
2022-05-12 05:58:47 +0530

    changed css

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -2,25 +2,16 @@ import React from 'react';
 import { AiFillCaretLeft, AiFillCaretRight } from 'react-icons/ai';
 
 const Pagination = () => {
-	const activeClass =
-		'px-2 rounded-full cursor-pointer bg-slate-500 focus:bg-slate-500';
-	const nonActiveClass =
-		'px-2 hover:rounded-full cursor-pointer hover:bg-slate-500 focus:bg-slate-500';
-
 	return (
-		<footer className=" fixed bottom-0 w-full text-white p-2 flex justify-center items-center">
+		<footer className=" fixed bottom-0 w-full text-white p-4 flex justify-center items-center">
 			<div className="flex items-center bg-cyan-800 rounded-full">
-				<span className={nonActiveClass}>
+				<div className="px-2 text-2xl hover:rounded-full cursor-pointer hover:bg-slate-500 focus:bg-slate-500">
 					<AiFillCaretLeft />
-				</span>
-				<span className={activeClass}>1</span>
-				<span className={nonActiveClass}>2</span>
-				<span className={nonActiveClass}>3</span>
-				<span className={nonActiveClass}>4</span>
-				<span className={nonActiveClass}>5</span>
-				<span className={nonActiveClass}>
+				</div>
+
+				<div className="px-2 text-2xl hover:rounded-full cursor-pointer hover:bg-slate-500 focus:bg-slate-500">
 					<AiFillCaretRight />
-				</span>
+				</div>
 			</div>
 		</footer>
 	);
